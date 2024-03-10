260815a501e3b352cbbbfbb07ad424b3ca81070e
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-03-10 18:45:38 +0200

    added better support for incorrect server response

--- frontend/src/components/Bus.tsx
+++ frontend/src/components/Bus.tsx
@@ -7,26 +7,31 @@ import { Trip } from "./Trip";
 export const Bus = () => {
     const stop = useSelector((state: IRootState) => state.bus);
 
+    const stopName = stop.name === "null" ? "No stop" : stop.name;
+    const stopCode = stop.code === "null" ? "?" : stop.code;
+
     return (
     <div className="item">
         <div>
             <div className="">
                 <h1 className="">Bus times</h1>
-                <h2 className="">{stop.name}: {stop.code}</h2>
+                <h2 className="">{stopName}: {stopCode}</h2>
             </div>
-            <table className="trips">
-                <thead>
-                    <tr>
-                        <th>Departure time</th>
-                        <th>Line number</th>
-                        <th>Line name</th>
-                        <th>Time to departure</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stop.trips.map(trip => <Trip key={trip.id} trip={trip} />)}
-                </tbody>
-            </table>
+            { stop.trips.length > 0 && 
+                <table className="trips">
+                    <thead>
+                        <tr>
+                            <th>Departure time</th>
+                            <th>Line number</th>
+                            <th>Line name</th>
+                            <th>Time to departure</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stop.trips.map(trip => <Trip key={trip.id} trip={trip} />)}
+                    </tbody>
+                </table>
+            }
         </div>
     </div>
     )
--- frontend/src/components/Tram.tsx
+++ frontend/src/components/Tram.tsx
@@ -7,26 +7,31 @@ import { Trip } from "./Trip";
 export const Tram = () => {
     const stop = useSelector((state: IRootState) => state.tram);
 
+    const stopName = stop.name === "null" ? "No stop" : stop.name;
+    const stopCode = stop.code === "null" ? "?" : stop.code;
+
     return (
     <div className="item">
         <div>
             <div className="">
                 <h1 className="">Tram times</h1>
-                <h2 className="">{stop.name}: {stop.code}</h2>
+                <h2 className="">{stopName}: {stopCode}</h2>
             </div>
-            <table className="trips">
-                <thead>
-                    <tr>
-                        <th>Departure time</th>
-                        <th>Line number</th>
-                        <th>Line name</th>
-                        <th>Time to departure</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stop.trips.map(trip => <Trip key={trip.id} trip={trip} />)}
-                </tbody>
-            </table>
+            { stop.trips.length > 0 && 
+                <table className="trips">
+                    <thead>
+                        <tr>
+                            <th>Departure time</th>
+                            <th>Line number</th>
+                            <th>Line name</th>
+                            <th>Time to departure</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stop.trips.map(trip => <Trip key={trip.id} trip={trip} />)}
+                    </tbody>
+                </table>
+            }
         </div>
     </div>
     )
